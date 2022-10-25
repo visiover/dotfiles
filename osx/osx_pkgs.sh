d8788edcbec8a435d4da6bf76d6088df4fee9947
Maher Mhiri <visiover13@gmail.com>
2022-10-25 09:18:42 +0200

    Updated MacOs Packages installer script

--- osx/osx_pkgs.sh
+++ osx/osx_pkgs.sh
@@ -8,7 +8,7 @@ xcode-select --install
 if ! type "brew" > /dev/null; then
     #install homebrew
 	echo "\n==> Installing Homebrew...\n"
-	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 else
 	echo "\nHomebrew installation found\nUpdating Homebrew "
 	# Make sure we’re using the latest Homebrew.
@@ -23,98 +23,72 @@ fi
 brew doctor
 
 # Add Homebrew completions
-brew tap homebrew/completions
+brew tap homebrew/homebrew-core
 
 
 # Install GNU `find`, `locate`, `updatedb`, and `xargs`, g-prefixed
 brew install findutils
 
 # Install more recent versions of some macOS tools.
-brew install vim --with-override-system-vi
-brew install homebrew/dupes/grep
-brew install homebrew/dupes/openssh
-brew install homebrew/dupes/screen
+brew install vim
+brew install grep
+brew install openssh
+brew install screen
 
 
 echo "Cleaning up Homebrew cache ..."
 brew cleanup
 
-#TODO : check if cask is installed and these command lines are correct
-echo "\n==> Installing Homebrew cask...\n" 
-brew tap caskroom/cask
-brew tap caskroom/versions
-
 
 CASKS=(
     sublime-text
-    istat-menus
     google-chrome
     google-drive
     firefox
-    skype
     vlc
-    utorrent
     spotify
     dropbox
     unrarx
     the-unarchiver
     spectacle
-    appcleaner
     iterm2
-    teamviewer
-    phpstorm
     libreoffice
-    filezilla
-    skype
-    google-drive
     slack
-    unity
-    android-file-transfer
-    wireshark
     marp
-    ccleaner
     sequel-pro
-    virtualbox
+    lastpass
+    dbngin
+    insomnia
+    visual-studio-code
+    sequel-ace
+    htop
 )
 
 #install all the needed apps
 echo "\n==> Installing all the needed packages\n"
-brew cask install ${CASKS[@]}
+brew install --cask ${CASKS[@]}
 
-
-# Some apps don't have a cask and so still need to be installed by hand. These include:
-    #memory clean 2
-    #uTorrent
-    #Duplicate detective
-    #Stellar Phoenix Mac Data Recovery 
-    #Traktor DJ
-    #Microsoft Office
-    #Guidance
 
 #install some fonts
 echo "Installing fonts..."
-brew tap caskroom/fonts
+brew tap homebrew/cask-fonts 
 
 FONTS=(
     font-inconsolidata
-    font-roboto
     font-clear-sans
 )
 
-brew cask install ${FONTS[@]}
+brew install ${FONTS[@]}
 
-# cleanup cask cache
-brew cask cleanup
+# cleanup cache
 brew cleanup
 
-# TODO: Write tests
 # TODO: Check if the packages have been successfully installed
-
 
 # Autohide dock without a delay
 defaults write com.apple.dock autohide-delay -float 0; killall Dock;
 
 
 #brew cask info pkg-name
-brew cask list
+brew list
 echo "\n\n==> All the packages have been installed :)  \n\n"
